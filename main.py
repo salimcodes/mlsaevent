dc636024126a34bac22db437545658089d1348d3
Salim Oyinlola <salimoyinlola@gmail.com>
2024-06-18 18:00:08 +0100

    Salim's column added

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("My name is Clinton and I am the host of this event")
+print("My name is Clinton, and I am the host of this event")
+print("My name is Salim, and I am the co-host of this event")
